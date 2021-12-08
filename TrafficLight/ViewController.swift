8e22029732f14b9ff2b02eed1c587a3a7ce867a4
Elisei Mishchenko <esm.mail@icloud.com>
2021-12-08 15:35:59 +0300

    Work with logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 10
+        startButton.titleLabel?.font = UIFont.systemFont(ofSize: 35)
+        redView.layer.cornerRadius = 80
+        redView.alpha = 0.5
+        yellowView.layer.cornerRadius = 80
+        yellowView.alpha = 0.5
+        greenView.layer.cornerRadius = 80
+        greenView.alpha = 0.5
     }
 
-
+    @IBAction func startPressed() {
+        if redView.alpha == 1 {
+            redView.alpha = 0.5
+            yellowView.alpha = 1
+        } else if yellowView.alpha == 1 {
+            yellowView.alpha = 0.5
+            greenView.alpha = 1
+        } else if redView.alpha == 0.5 {
+            startButton.setTitle("NEXT", for: .normal)
+            greenView.alpha = 0.5
+            redView.alpha = 1
+        }
+    }
+    
 }
 
